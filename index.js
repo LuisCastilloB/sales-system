498044e64f1ac9f426b6eca9ded49a27c3cd95fa
Luis Castillo <lacb93@gmail.com>
2020-01-06 18:17:44 -0500

    Add middlewares at proyect, like cors & morgan

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
 const app = express();
+//Allow debug request 
+app.use(morgan('dev'));
+//Allow debug request in production enviroment
+app.use(cors());
+//Allow analys body data request for messages send by client
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
 app.set('port', process.env.PORT || 3000);
 
 
 app.listen(app.get('port'), () => {
-    console.log("Servidor corriendo por el puerto " + app.get('port'));
+    console.log("Running server in port: " + app.get('port'));
 });
